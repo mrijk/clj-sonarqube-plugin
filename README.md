a9e3d0a39ebdd00df6613cc18f285b8d45eb0d1f
Maurits <maurits.rijk@gmail.com>
2016-08-20 13:29:27 +0200

    Update license

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # clj-sonarqube-plugin
-Sample plugin for SonarQube written in Clojure
+Sample plugin for SonarQube written in Clojure.
+
+It is tested with a Docker image of SonarQube 6.0.
+
+docker pull sonarqube
+
+docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+
+To build and install the plugin:
+
+./build.sh
+
+
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject clj-sonar "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Sample plugin for SonarQube written in Clojure"
+  :url "https://github.com/mrijk/clj-sonarqube-plugin"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.sonarsource.sonarqube/sonar-plugin-api, "5.6"]]
   :manifest {"Plugin-Key" "clj-example"
